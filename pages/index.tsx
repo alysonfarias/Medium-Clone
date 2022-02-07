3bfea78645c823f64d76bc870ae877b08a18034d
Alyson <ramos.alysonfarias@gmail.com>
2022-02-06 21:38:31 -0300

    updated title page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home( posts : Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Medium Blog v0.3</title>
+        <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
